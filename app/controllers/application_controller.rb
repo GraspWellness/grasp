cb22f080f31b0527be03a912d86f2dfbe792c620
Paris Yee <paris.yee@gmail.com>
2017-09-05 11:59:16 -0400

    Require basic auth on staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :require_basic_auth_if_staging!
   protect_from_forgery with: :exception
+
+  private
+  
+  def require_basic_auth_if_staging!
+    if Rails.env.staging?
+      http_basic_authenticate_with(
+        name: ENV["BASIC_AUTH_USERNAME"],
+        password: ENV["BASIC_AUTH_PASSWORD"]
+      )
+    end
+  end
 end
