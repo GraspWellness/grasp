ced144892b013dfbe45c4d47d9e9860976c9e7b2
Paris Yee <paris.yee@gmail.com>
2017-10-17 22:49:45 -0400

    Refactor admin welcome email

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,6 +4,10 @@ class AdminMailer < ApplicationMailer
   def welcome(admin, reset_password_token)
     @reset_password_url = edit_admin_password_url(reset_password_token: reset_password_token)
     @admin = admin
-    mail(to: @admin.email, subject: "Welcome to Grasp Admin Portal!")
+    mail(
+      to: @admin.email,
+      from: "admin@#{ActionMailer::Base.default_url_options[:host]}",
+      subject: "Welcome to Grasp Admin Portal!"
+    )
   end
 end
